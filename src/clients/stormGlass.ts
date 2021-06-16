20304223b46d495e3a79199f7fa0893f23fa998f
Luixinho <ticolucas7@gmail.com>
2021-06-16 18:16:27 -0300

    api data normalization

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,15 +1,77 @@
 import { AxiosStatic } from 'axios';
 
+export interface StormGlassPointSource {
+  [key: string]: number; // key ali dentro representa que a chave do array (valor principal, geralmente o primeiro) vai ser uma string isso torna dinâmico, qualquer string que entre ali sera tipada como number
+}
+
+export interface StormGlassPoint {
+  readonly time: string;
+  readonly waveHeight: StormGlassPointSource;
+  readonly waveDirection: StormGlassPointSource;
+  readonly swellDirection: StormGlassPointSource;
+  readonly swellHeight: StormGlassPointSource;
+  readonly swellPeriod: StormGlassPointSource;
+  readonly windDirection: StormGlassPointSource;
+  readonly windSpeed: StormGlassPointSource;
+}
+
+export interface StormGlassForecastResponse {
+  hours: StormGlassPoint[];
+}
+
+export interface ForecastPoint {
+  time: string;
+  waveHeight: number;
+  waveDirection: number;
+  swellDirection: number;
+  swellHeight: number;
+  swellPeriod: number;
+  windDirection: number;
+  windSpeed: number;
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
-  readonly stormGlassAPISource = 'noaaa';
+  readonly stormGlassAPISource = 'noaa';
 
   constructor(protected request: AxiosStatic) {}
 
-  public async fetchPoints(lat: number, lng: number): Promise<{}> {
-    return this.request.get(
+  public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
+    // dentro das <> está setado o tipo da resposta que virá desse método get
+    const response = await this.request.get<StormGlassForecastResponse>(
       `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}`
+    );
+    return this.normalizeResponse(response.data);
+  }
+  private normalizeResponse(
+    points: StormGlassForecastResponse
+  ): ForecastPoint[] {
+    return points.hours.filter(this.isValidPoint.bind(this)).map((point) => ({
+      swellDirection: point.swellDirection[this.stormGlassAPISource],
+      swellHeight: point.swellHeight[this.stormGlassAPISource],
+      swellPeriod: point.swellPeriod[this.stormGlassAPISource],
+      time: point.time,
+      waveDirection: point.waveDirection[this.stormGlassAPISource],
+      waveHeight: point.waveHeight[this.stormGlassAPISource],
+      windDirection: point.windDirection[this.stormGlassAPISource],
+      windSpeed: point.windSpeed[this.stormGlassAPISource],
+    }));
+  }
+
+  // partial faz com que as propriedades possam ser opcionais ou undefined
+  private isValidPoint(point: Partial<StormGlassPoint>): boolean {
+    // os dois pontos de exclamação força o retorno dessa função ser um boolean
+    // ?. faz uma confirmação se a chave citada antes possui o indice do objeto mencionado posteriormente
+    return !!(
+      point.time &&
+      point.swellDirection?.[this.stormGlassAPISource] &&
+      point.swellHeight?.[this.stormGlassAPISource] &&
+      point.swellPeriod?.[this.stormGlassAPISource] &&
+      point.waveDirection?.[this.stormGlassAPISource] &&
+      point.waveHeight?.[this.stormGlassAPISource] &&
+      point.windDirection?.[this.stormGlassAPISource] &&
+      point.windSpeed?.[this.stormGlassAPISource]
     );
   }
 }
