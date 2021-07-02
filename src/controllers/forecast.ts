92b6ee5057ca7ce1e7eaf4d7021e307c6cccc833
Luixinho <ticolucas7@gmail.com>
2021-07-02 18:07:09 -0300

    Logger implemented

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { Forecast } from '@src/services/forecast';
 import { Beach } from '@src/models/beach';
 import { authMiddleware } from '@src/middlewares/auth';
+import logger from '@src/logger';
 
 const forecast = new Forecast();
 // decorators (comandos começando com @ são atalhos para definir rotas colocados acima do que deve ser chamado quando acessada a rota definida após o @)
@@ -19,6 +20,7 @@ export class ForecastController {
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
     } catch (error) {
+      logger.error(error);
       res.status(500).send({ error: 'Something went wrong' });
     }
   }
