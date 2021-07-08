982aa90c7d40ae3c9dc0eb9d53a09007c6f502d9
Luixinho <ticolucas7@gmail.com>
2021-07-08 16:25:31 -0300

    Beach rating system

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,9 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from './index';
 import AuthService from '@src/services/auth';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -18,16 +19,14 @@ export class UsersController extends BaseController {
   }
 
   @Post('authenticate')
-  public async authenticate(
-    req: Request,
-    res: Response
-  ): Promise<Response | undefined> {
+  public async authenticate(req: Request, res: Response): Promise<Response> {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
       return this.sendErrorResponse(res, {
         code: 401,
         message: 'User not found!',
+        description: 'Try verifying your email address.',
       });
     }
     if (!(await AuthService.comparePasswords(password, user.password))) {
@@ -37,6 +36,21 @@ export class UsersController extends BaseController {
       });
     }
     const token = AuthService.generateToken(user.toJSON());
-    return res.status(200).send({ token: token });
+
+    return res.send({ ...user.toJSON(), ...{ token } });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+    return res.send({ user });
   }
 }
